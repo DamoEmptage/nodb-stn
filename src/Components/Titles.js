d0e05488610071c7bab207d4f6517e60f67e61a9
Damon Emptage <dadbodtv@gmail.com>
2019-02-09 13:05:09 -0700

    working

---
+++ src/Components/Titles.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Titles = props => (
+  <div>
+    <h1 className='title-containter__title'>Survive the night</h1>
+    <h3 className='title-container__subtitle'>Whats Lurking Outside </h3>
+  </div>
+)
+
+export default Titles;
